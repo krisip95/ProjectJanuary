87d6114717b9f1a8efc6aee9cf19ee8eba48cf07
krisip95 <71447845+krisip95@users.noreply.github.com>
2021-01-18 14:19:54 +0200

    Update Juice1_GUI.java

--- Juice1_GUI.java
+++ Juice1_GUI.java
@@ -11,8 +11,7 @@ public class Juice1_GUI extends Order1_GUI {
 		try {
 			fruit = (String) JOptionPane.showInputDialog(label,"Select a fruit.","Select a fruit",
 					JOptionPane.QUESTION_MESSAGE,null,fruits,fruits[0]);
-			//Creates a pop up that asks to the user, from which fruit wants the juice. Takes label as a paramater to display the pop up on the frame
-			//If he clicks on "OK" following codes will be executed.
+			
 			double price;
 			if(size.equals("Small")) price=5.0;
 			else if(size.equals("Medium")) price=7.0;
